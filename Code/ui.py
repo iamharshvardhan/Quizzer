b30d62d0885113c72c59c8287388c7d8d729b1ab
Harshvardhan Rana <ranaharshvardhan52@gmail.com>
2022-07-27 02:55:32 +0530

    First commit

---
+++ Code/ui.py
@@ -0,0 +1,60 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+THEME_COLOR = "#375362"
+
+
+class QuizInterface:
+
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+        self.window = Tk()
+        self.window.title("Quizzer")
+        self.window.config(pady=10, padx=10, bg=THEME_COLOR)
+
+        self.score_label = Label()
+        self.score_label.config(text="Score : 0", bg=THEME_COLOR, fg="white")
+        self.score_label.grid(column=1, row=0)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(150, 125,
+                                                     width=280, text=f"some question here",
+                                                     font=("Ariel", 15, "italic"),
+                                                     fill=THEME_COLOR)
+        self.canvas.grid(column=0, row=1, columnspan=2, padx=20, pady=20)
+
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_image, highlightthickness=0, command=self.true_pressed)
+        self.true_button.grid(column=0, row=2)
+
+        false_photo = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_photo, highlightthickness=0, command=self.false_pressed)
+        self.false_button.grid(column=1, row=2)
+        self.get_next_question()
+
+        self.window.mainloop()
+
+    def get_next_question(self):
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="This is the End of the Quiz!")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+
+    def true_pressed(self):
+        is_right = self.quiz.check_answer("True")
+        self.give_feedback(is_right)
+
+    def false_pressed(self):
+        self.give_feedback(self.quiz.check_answer("False"))
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.canvas.after(500, func=self.get_next_question)
+        self.score_label.config(text=f"Score: {self.quiz.score}")
+
---
+++ README.md
@@ -0,0 +1,5 @@
+# Quizzer APP
+
+## Description
+This is a Quiz app with tkinter GUI interface.
+It has 10 True/Flase questions to Answer.
